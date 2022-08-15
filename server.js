5a56e0b6a16ee9e3439cb3e61c71bea7e986c439
blessing332 <blessinganikwe@gmail.com>
2022-08-15 02:20:02 -0500

    updating server.js

--- server.js
+++ server.js
@@ -45,20 +45,93 @@ app.use(express.json());// returns middleware that only parses JSON - may or may
 //use method override
 app.use(methodOverride('_method'));// allow POST, PUT and DELETE from a form
 
+////////////// Create DB //////////////////
+// app.get('/datedb', (req, res)=>{
+//   Date.create(profileSeed, (err, data) => {
+//       res.send(data);
+// })
+//})
+//  Date.collection.drop()
+  //
+  Date.countDocuments({}, (err, data) => {
+    if (err){ console.log(err.message)}
+    else {
+    console.log(`There are ${data} in this database`)}
+  })
+///////////////////////////////////
 
 //___________________
 // Routes
 //___________________
 //localhost:3000
-app.get('/' , (req, res) => {
-  res.send('Hello World!');
-});
+// app.get('/' , (req, res) => {
+//   res.send('Hello World!');
+// });
 
+app.put('/datedb/:id', (req, res) => {
+    if (req.body.isInterested === 'on') {
+        req.body.isInterested = true;
+    } else {
+        req.body.isInterested = false;
+    }
+
+    Date.findByIdAndUpdate(req.params.id, req.body, {new:true}, (error, updateModel) => {
+        res.redirect('/datedb')
+    })
+})
+
+app.get('/datedb/:id/edit', (req, res) => {
+    Date.findById(req.params.id, (error, foundDates) => {
+    res.render(
+        'edit.ejs', { name: foundDates })
+    })
+})
+
+app.delete('/datedb/:id', (req, res) => {
+    Date.findByIdAndRemove(req.params.id, (error, data) => {
+        res.redirect('/datedb');
+    })
+    // res.send('deleting...')
+})
+
+app.get('/datedb/new', (req, res)=>{
+  res.render('new.ejs')
+})
+
+app.get('/datedb', (req, res) => {
+  //res.send("working")
+    Date.find({}, (error, allDates) => {
+      console.log(allDates);
+        res.render(
+             'index.ejs', { name: allDates },
+         )
+     })
+})
+app.post('/datedb', (req, res) => {
+    Date.create(req.body, (error, createdDates) => {
+        if (req.body.isInterested === 'on') {
+            req.body.isInterested = true;
+        } else {
+            req.body.isInterested = false;
+        }
+        Date.create(req.body, (error, createdDates) => {
+            res.redirect('/datedb')
+        })
+    })
+})
+
+// app.get('/datedb/:id', (req, res) => {
+//     Date.findById(req.params.id, (error, foundDate) => {
+//         res.render(
+//             'show.ejs', { budget: foundDate }
+//         )
+//     })
+// })
 //___________________
 //Listener
 //___________________
 app.listen(PORT, () => console.log( 'Listening on port:', PORT));
 
-mongoose.connect('mongodb://127.0.0.1:27017/full-stack-application', () => {
+mongoose.connect('mongodb://127.0.0.1:27017/Full-Stack-Application', () => {
     console.log('The connection with mongod is established')
   })
